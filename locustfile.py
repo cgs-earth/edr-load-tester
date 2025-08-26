63ea421d6a9ae21c5c4bcff2ae1dc67740759789
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-08-26 11:11:44 -0400

    backup

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,9 @@
+from functools import partial
+import logging
 from locust import HttpUser, TaskSet, constant, run_single_user, task, events
 from locust.clients import HttpSession
 import gevent
+from gevent.pool import Group
 
 from edr_client_models import CollectionItem, TopLevelCollectionResponse
 
@@ -43,7 +46,25 @@ class EDRHttpTesterUser(TaskSet):
                 for query in data_queries:
                     if query == "locations":
                         link = data_queries[query]["link"]
-                        self.client.get(link["href"], name=f"/collections/{collection['id']}/{query}")
+                        location_response = self.client.get(link["href"], name=f"/collections/{collection['id']}/{query}")
+                        asJson = location_response.json()
+                        group = Group()
+                        MAX_LOCATION_CHECKS = 5
+                        for location in asJson["features"]:
+                            location_id = location["id"]
+
+                            def run_fetch(collection_id: str, location_id: str ):
+                                new_link = link["href"] + f"/{location_id}"
+                                logging.info(f"Fetching {new_link}")
+                                self.client.get(new_link, name=f"/collections/{collection_id}/locations/{location_id}")
+
+                            # run locations in parallel to add load
+                            group.spawn(partial(run_fetch, collection_id=collection["id"], location_id=location_id))
+                            MAX_LOCATION_CHECKS -= 1
+                            if MAX_LOCATION_CHECKS == 0:
+                                break
+
+                        group.join()  # wait for greenlets to finish
 
                     if query == "items":
                         link = data_queries[query]["link"]
